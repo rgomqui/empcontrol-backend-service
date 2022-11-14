be744f4e067a739534ebd1ff55f6099943be0f55
rgomezqu <rgomqui479@gmail.com>
2022-11-14 22:03:55 +0100

    feat: first commit

---
+++ src/test/kotlin/com/elma/empcontrol/EmpcontrolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.elma.empcontrol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmpcontrolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
